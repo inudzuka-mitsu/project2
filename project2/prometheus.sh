44aada107afb4954b7de971c1586bf4684af59c8
inudzuka-mitsu <valeriiababina.qa@gmail.com>
2024-04-22 23:54:46 +0000

    node exporter installation

--- project2/prometheus.sh
+++ project2/prometheus.sh
@@ -2,12 +2,15 @@
 sudo apt-get update
 sudo apt-get install -y curl tar
 
-# Creating Prometheus User
+# Creating Prometheus User and Node exporter user
 sudo useradd --no-create-home --shell /bin/false prometheus
+
+sudo useradd --no-create-home --shell /bin/false node_exporter
 
 # Creating directories
 sudo mkdir /etc/prometheus
 sudo mkdir /var/lib/prometheus
+sudo mkdir /etc/node_exporter
 
 # Setting ownership
 sudo chown prometheus:prometheus /etc/prometheus
@@ -33,6 +36,20 @@ sudo cp -r console_libraries /etc/prometheus
 sudo cp prometheus.yml /etc/prometheus/prometheus.yml
 sudo chown -R prometheus:prometheus /etc/prometheus
 
+# Downloading Node exporter
+cd /tmp/
+wget https://github.com/prometheus/node_exporter/releases/download/v1.3.1/node_exporter-1.3.1.linux-amd64.tar.gz
+
+# Unpacking
+tar xvf node_exporter-1.3.1.linux-amd64.tar.gz
+cd node_exporter-1.3.1.linux-amd64/
+
+# Copying binaries
+sudo cp node_exporter /usr/local/bin/
+
+# Copying configuration files and setting ownership
+sudo chown node_exporter:node_exporter /usr/local/bin/node_exporter
+
 # Creating Prometheus service file
 echo '[Unit]
 Description=Prometheus
@@ -48,7 +65,24 @@ ExecStart=/usr/local/bin/prometheus --config.file=/etc/prometheus/prometheus.yml
 [Install]
 WantedBy=multi-user.target' | sudo tee /etc/systemd/system/prometheus.service
 
+# Create service file for Node Exporter
+echo '[Unit]
+Description=Node Exporter
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+User=node_exporter
+Group=node_exporter
+Type=simple
+ExecStart=/usr/local/bin/node_exporter
+
+[Install]
+WantedBy=multi-user.target' |  sudo tee /etc/systemd/system/node_exporter.service
+
 # Reloading systemd to apply changes
 sudo systemctl daemon-reload
 sudo systemctl enable prometheus
 sudo systemctl start prometheus
+sudo systemctl enable node_exporter
+sudo systemctl start node_exporter
