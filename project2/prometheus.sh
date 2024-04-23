8ff37da385fdf8d2231ad705f6fe6326b4d2f95b
inudzuka-mitsu <valeriiababina.qa@gmail.com>
2024-04-23 17:27:21 +0000

    installed alert manager

--- project2/prometheus.sh
+++ project2/prometheus.sh
@@ -2,55 +2,70 @@
 sudo apt-get update
 sudo apt-get install -y curl tar
 
-# Creating Prometheus User and Node exporter user
+# Creating users
 sudo useradd --no-create-home --shell /bin/false prometheus
-
 sudo useradd --no-create-home --shell /bin/false node_exporter
+sudo useradd --no-create-home --shell /bin/false alertmanager
 
 # Creating directories
 sudo mkdir /etc/prometheus
 sudo mkdir /var/lib/prometheus
 sudo mkdir /etc/node_exporter
+sudo mkdir /etc/alertmanager
+sudo mkdir /var/lib/alertmanager
 
 # Setting ownership
 sudo chown prometheus:prometheus /etc/prometheus
 sudo chown prometheus:prometheus /var/lib/prometheus
+sudo chown alertmanager:alertmanager /etc/alertmanager
+sudo chown alertmanager:alertmanager /var/lib/alertmanager
 
-# Downloading Prometheus
+# Download and install Prometheus
 cd /tmp/
 curl -LO https://github.com/prometheus/prometheus/releases/download/v2.37.0/prometheus-2.37.0.linux-amd64.tar.gz
-
-# Unpacking Prometheus
 tar xvf prometheus-2.37.0.linux-amd64.tar.gz
 cd prometheus-2.37.0.linux-amd64/
-
-# Copying binaries
 sudo cp prometheus /usr/local/bin/
 sudo cp promtool /usr/local/bin/
 sudo chown prometheus:prometheus /usr/local/bin/prometheus
 sudo chown prometheus:prometheus /usr/local/bin/promtool
-
-# Copying configuration files and setting ownership
 sudo cp -r consoles /etc/prometheus
 sudo cp -r console_libraries /etc/prometheus
 sudo cp prometheus.yml /etc/prometheus/prometheus.yml
 sudo chown -R prometheus:prometheus /etc/prometheus
 
-# Downloading Node exporter
+# Download and install Node Exporter
 cd /tmp/
 wget https://github.com/prometheus/node_exporter/releases/download/v1.3.1/node_exporter-1.3.1.linux-amd64.tar.gz
-
-# Unpacking
 tar xvf node_exporter-1.3.1.linux-amd64.tar.gz
 cd node_exporter-1.3.1.linux-amd64/
-
-# Copying binaries
 sudo cp node_exporter /usr/local/bin/
-
-# Copying configuration files and setting ownership
 sudo chown node_exporter:node_exporter /usr/local/bin/node_exporter
 
-# Creating Prometheus service file
+# Download and install Alertmanager
+cd /tmp/
+wget https://github.com/prometheus/alertmanager/releases/download/v0.26.0/alertmanager-0.26.0.linux-amd64.tar.gz
+tar xvf alertmanager-0.26.0.linux-amd64.tar.gz
+cd alertmanager-0.26.0.linux-amd64/
+sudo cp alertmanager /usr/local/bin/
+sudo cp amtool /usr/local/bin/
+sudo chown alertmanager:alertmanager /usr/local/bin/alertmanager
+sudo chown alertmanager:alertmanager /usr/local/bin/amtool
+echo "global:
+  resolve_timeout: 5m
+route:
+  group_by: ['alertname']
+  group_wait: 10s
+  group_interval: 10m
+  repeat_interval: 1h
+  receiver: 'web.hook'
+receivers:
+- name: 'web.hook'
+  webhook_configs:
+  - url: 'http://example.com/hook'" | sudo tee /etc/alertmanager/alertmanager.yml
+sudo chown -R alertmanager:alertmanager /etc/alertmanager
+
+# Creating systemd service files
 echo '[Unit]
 Description=Prometheus
 Wants=network-online.target
@@ -65,7 +80,6 @@ ExecStart=/usr/local/bin/prometheus --config.file=/etc/prometheus/prometheus.yml
 [Install]
 WantedBy=multi-user.target' | sudo tee /etc/systemd/system/prometheus.service
 
-# Create service file for Node Exporter
 echo '[Unit]
 Description=Node Exporter
 Wants=network-online.target
@@ -78,11 +92,28 @@ Type=simple
 ExecStart=/usr/local/bin/node_exporter
 
 [Install]
-WantedBy=multi-user.target' |  sudo tee /etc/systemd/system/node_exporter.service
+WantedBy=multi-user.target' | sudo tee /etc/systemd/system/node_exporter.service
 
-# Reloading systemd to apply changes
+echo '[Unit]
+Description=Alertmanager
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+User=alertmanager
+Group=alertmanager
+Type=simple
+ExecStart=/usr/local/bin/alertmanager --config.file=/etc/alertmanager/alertmanager.yml --storage.path=/var/lib/alertmanager/
+
+[Install]
+WantedBy=multi-user.target' | sudo tee /etc/systemd/system/alertmanager.service
+
+# Reloading systemd, enabling and starting services
+
 sudo systemctl daemon-reload
 sudo systemctl enable prometheus
 sudo systemctl start prometheus
 sudo systemctl enable node_exporter
 sudo systemctl start node_exporter
+sudo systemctl enable alertmanager
+sudo systemctl start alertmanager
